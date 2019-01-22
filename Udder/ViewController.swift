c1fe91bae2ebee61c4b688f9d251b3e47b7f9d7e
Taisei Mogi <makkasa09@gmail.com>
2019-01-22 23:09:54 +0900

    Initial Commit

---
+++ Udder/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Udder
+//
+//  Created by TaiseiMogi on 2019/01/22.
+//  Copyright © 2019年 TaiseiMogi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
